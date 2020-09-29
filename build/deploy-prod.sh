58dd524e019b59890d42aa6cdf3213033f12ce37
Rattan Pal Kaur <rattan.kaur@BRILLIO.COM>
2020-09-29 21:26:23 +0530

    Modified few files

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -2,5 +2,6 @@
 echo "Converting to MDAPI format..."
 sfdx force:source:convert -d deploy_prod -r force-app 
 #Deploy to prod & run all tests
-echo "Deploying to production & running all tests..."
-sfdx force:mdapi:deploy -u DevHub -d ./manifest -w -1 -l RunAllTestsInOrg b 
+echo "Deploying to production"
+#sfdx force:mdapi:deploy -u DevHub -d ./manifest -w -1 -l RunAllTestsInOrg b 
+sfdx force:source:deploy -x manifest/package.xml -w 2 -u DevHub
