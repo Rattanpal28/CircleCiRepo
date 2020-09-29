a4355beb6fd423da476e46f46e97819af4138938
Rattan Pal Kaur <rattan.kaur@BRILLIO.COM>
2020-09-29 20:31:27 +0530

    Updated deploy-qa.sh

--- build/deploy-qa.sh
+++ build/deploy-qa.sh
@@ -3,5 +3,6 @@
 #sfdx force:source:convert -d deploy_qa -r force-app
 #Deploy to prod & run all tests
 echo "Deploying metadata api"
+sfdx force:source:convert -d deploy_qa -r force-app 
 echo "Deploying to production & running all tests..."
 sfdx force:mdapi:deploy -u Qat -d ./manifest -w 100 
